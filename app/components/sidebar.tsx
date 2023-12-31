ad150bb524cfc144648a245b9cd1741135d319e5
anthonyshibo <133836502+anthonyshibo@users.noreply.github.com>
2023-12-31 19:25:27 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          安东尼的AI助手
         </div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
